988edb5bff51d2cb95d3ce4b6171ea791b811285
Paolo Gomez <78209142+pattherogue@users.noreply.github.com>
2021-08-25 11:35:40 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,2 +1,9 @@
 /* Get and display 12 random users */
 fetch('https://randomuser.me/api/?results=12')
+    /* data from Fetch API */
+    .then((response) => response.json())
+    /* pass data to function */
+    .then(data => {
+        profileHTML(data.results);
+        clickModal(data.results);
+    })
