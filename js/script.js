89a304fbc3bc1f73932ee8f90ebee8ae60750e61
Paolo Gomez <78209142+pattherogue@users.noreply.github.com>
2021-08-27 05:43:53 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -9,7 +9,26 @@ fetch('https://randomuser.me/api/?results=12')
     })
 
 function generateProfile(data) {
-    data.map => {
+    for (let i = 0; i < data.length; i++) {
+       const user = 
+        `<div class="card">
+        <div class="card-img-container">
+            <img class="card-img" src="${user[i].picture.large}" alt="profile picture">
+            </div>
+        <div class="card-info-container">
+            <h3 id="name" class="card-name cap">${user[i].name.first} ${user[i].name.last}</h3>
+            <p class="card-text">${user[i].email}</p>
+            <p class="card-text cap">${user[i].location.city}, ${user[i].location.state}</p>
+            </div>
+            </div>`; 
+
+    
+        
+
+
+
+
+    }
         
     }
 }
