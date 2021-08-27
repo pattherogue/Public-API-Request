a78e13d5c7b46a9dbb554219d58b833fa5752526
Paolo Gomez <78209142+pattherogue@users.noreply.github.com>
2021-08-27 06:16:41 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -9,26 +9,20 @@ fetch('https://randomuser.me/api/?results=12')
     })
 
 function generateProfile(data) {
-    for (let i = 0; i < data.length; i++) {
-       const user = 
+    const profile = data.map(user => { 
         `<div class="card">
         <div class="card-img-container">
-            <img class="card-img" src="${user[i].picture.large}" alt="profile picture">
+            <img class="card-img" src="${user.picture.large}" alt="profile picture">
             </div>
         <div class="card-info-container">
-            <h3 id="name" class="card-name cap">${user[i].name.first} ${user[i].name.last}</h3>
-            <p class="card-text">${user[i].email}</p>
-            <p class="card-text cap">${user[i].location.city}, ${user[i].location.state}</p>
+            <h3 id="name" class="card-name cap">${user.name.first} ${user.name.last}</h3>
+            <p class="card-text">${user.email}</p>
+            <p class="card-text cap">${user.location.city}, ${user.location.state}</p>
             </div>
-            </div>`; 
+        </div>`; 
 
-    
+    const gallery = document.getElementById('gallery');
+    gallery.insertAdjacentHTML('beforeend', profile);
+    });
         
-
-
-
-
-    }
-        
-    }
 }
