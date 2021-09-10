3b88987c61583643552642566dfb69231eab4d66
Paolo Gomez <78209142+pattherogue@users.noreply.github.com>
2021-09-10 08:32:15 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ fetch('https://randomuser.me/api/?results=12')
 
 function generateProfile(data) {
     /* create new array populated by calling provided function */
-    const profile = data.map(profile => { 
+    const html = data.map(profile => 
         /* template literal plus index.html file */
         `<div class="card">
         <div class="card-img-container">
@@ -26,37 +26,40 @@ function generateProfile(data) {
             <p class="card-text">${profile.email}</p>
             <p class="card-text cap">${profile.location.city}, ${profile.location.state}</p>
             </div>
-        </div>`;
+        </div>`).join(' ');
     
     /* parse text as HTML and specifies position */
-    gallery.insertAdjacentHTML('beforeend', profile);
-    });
+    gallery.insertAdjacentHTML('beforeend', html);
+ 
         
 }
 
 /* Create a modal window */
-function modalTemplate(index) {
-    let directory = directoryList.results[index];
-    const regexCell = /^\D*(\d{3})\D*(\d{3})\D*(\d{4})\D*$/
-    let formatCell = (directory.cell).replace(regexCell, '($1) $2-$3');
-    const regexDate = /(\d{4})-(\d{2})-(\d{2}).*/
-    let dateStructure = (directory.dob.date).replaced(regexDate, '$2/$3/$1')
+function modalTemplate(data) {
+    let birthday = data.dob.date;
+    let name = data.name.first;
+    let email = data.email;
+    let year = birthday.slice(0, 4);
+    let month = birthday.slice(5, 7);
+    let day = birthday.slice(9, 10);
 
     const modal = `
-    <div class="modal-container" data-index="${index}">
+    <div class="modal-container">
       <div class="modal" style="padding-bottom: 0px">
         <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
         <div class="modal-info-container" style="background: rgba(143, 119, 191, .8); height: 430px">
-          <img class="modal-img" src="${directory.picture.large}" alt="profile picture">
-          <h3 id="name" class="modal-name cap">${directory.name.first} ${directory.name.last}</h3>
-          <p class="modal-text">${directory.email}</p>
-          <p class="modal-text cap">${directory.location.city}</p>
+          <img class="modal-img" src="${data.picture.large}" alt="profile picture">
+          <h3 id="name" class="modal-name cap">${data.name.first} ${data.name.last}</h3>
+          <p class="modal-text">${data.email}</p>
+          <p class="modal-text cap">${data.location.city}</p>
           <hr>
-          <p class="modal-text">${formatCell}</p>
-          <p class="modal-text">${direco.location.street.number} ${directory.location.street.name}, ${directory.location.city}, ${directory.location.state} ${directory.location.postcode}</p>
-          <p class="modal-text">Birthday: ${dateStructure}</p>
+          <p class="modal-text">${data.cell}</p>
+          <p class="modal-text">${data.location.street.number} ${directory.location.street.name}, ${directory.location.city}, ${directory.location.state} ${directory.location.postcode}</p>
+          <p class="modal-text">Birthday: ${month} / ${day} / ${year}</p>
         </div>
       </div>
     </div>
     `;
+
+    
 }
