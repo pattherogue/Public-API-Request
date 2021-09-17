f326b270a3112f47ecfbdffba204a7cea70bf36e
Paolo Gomez <78209142+pattherogue@users.noreply.github.com>
2021-09-17 10:01:43 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const gallery = document.getElementById('gallery');
-const body = document.querySelector('body');
+
 
 
 /* Fetch and display 12 random users */
@@ -8,8 +8,8 @@ fetch('https://randomuser.me/api/?results=12')
     .then(response => response.json())
     .then(data => { 
       generateProfile(data.results);
-      modalDisplay(data.results);
-    })
+      modalClick(data.results);
+    });
 
  
 
@@ -17,6 +17,7 @@ fetch('https://randomuser.me/api/?results=12')
 function generateProfile(data) {
     /* create new array populated by calling provided function */
     data.map(profile => {
+
       profile = 
         `<div class="card">
         <div class="card-img-container">
@@ -27,7 +28,7 @@ function generateProfile(data) {
             <p class="card-text">${profile.email}</p>
             <p class="card-text cap">${profile.location.city}, ${profile.location.state}</p>
             </div>
-        </div>`
+        </div>`;
      
         gallery.insertAdjacentHTML('beforeend', profile);
       });
@@ -36,21 +37,21 @@ function generateProfile(data) {
 
 
 /* Create a modal for selected employee */
-function modalDisplay(data) {
+function modalDisplay(individualData) {
    
     const modal = `
     <div class="modal-container">
-      <div class="modal" style="padding-bottom: 0px">
+      <div class="modal">
         <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
         <div class="modal-info-container" style="background: rgba(143, 119, 191, .8); height: 430px">
-          <img class="modal-img" src="${data.picture.large}" alt="profile picture">
-          <h3 id="name" class="modal-name cap">${data.name.first} ${data.name.last}</h3>
-          <p class="modal-text">${data.email}</p>
-          <p class="modal-text cap">${data.location.city}</p>
+          <img class="modal-img" src="${individualData.picture.large}" alt="profile picture">
+          <h3 id="name" class="modal-name cap">${individualData.name.first} ${individualData.name.last}</h3>
+          <p class="modal-text">${individualData.email}</p>
+          <p class="modal-text cap">${individualData.location.city}</p>
           <hr>
-          <p class="modal-text">(${data.cell.slice(0, 3)}) ${data.cell.slice(4, 7)}-${data.cell.slice(8, 12)}</p>
-          <p class="modal-text">${data.location.street.number} ${data.location.street.name}, ${data.location.city}, ${data.location.state} ${data.location.postcode}</p>
-          <p class="modal-text">Birthday: ${data.dob.date.slice(5, 7)}/${data.dob.date.slice(8, 10)}/${data.dob.date.slice(0, 4)}</p>
+          <p class="modal-text">(${individualData.cell.slice(0, 3)}) ${individualData.cell.slice(4, 7)}-${individualData.cell.slice(8, 12)}</p>
+          <p class="modal-text">${individualData.location.street.number} ${individualData.location.street.name}, ${individualData.location.city}, ${individualData.location.state} ${individualData.location.postcode}</p>
+          <p class="modal-text">Birthday: ${individualData.dob.date.slice(5, 7)}/${individualData.dob.date.slice(8, 10)}/${individualData.dob.date.slice(0, 4)}</p>
         </div>
       </div>
     </div>`;
@@ -59,7 +60,7 @@ function modalDisplay(data) {
 }
 
 const buttonClose = document.getElementById('modal-close-btn');
-const containerModal = doucment.querySelector('.modal-container');
+const containerModal = document.querySelector('.modal-container');
 
 buttonClose.addEventListener('click', e => {
   containerModal.style.modalDisplay = 'none';
@@ -67,11 +68,12 @@ buttonClose.addEventListener('click', e => {
   });
 
 
+
 function modalClick(modalData) {
   const card = document.querySelector('.card');
-  for (let i = 0; i < card.clientHeight; i++) {
+  for (let i = 0; i < card.length; i++) {
     card[i].addEventListener('click', e => {
-      htmlModal(modalData[i]);
-    })
+      generateProfile(modalData[i]);
+    });
   }
 }
